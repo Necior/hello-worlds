9550f185e96d767b9cc253f2de4e5705fa614e21
Adrian Sadłocha <adrian@sadlocha.eu>
2015-01-25 09:57:12 +0100

    Change tabs to spaces

--- c_sharp.cs
+++ c_sharp.cs
@@ -1,10 +1,10 @@
 namespace HelloApp {
-	class HelloClass {
-		static void Main(string[] args) {
-			System.Console.WriteLine("Hello, what's your name?");
-			System.Console.Write("> ");
-			string name = System.Console.ReadLine();
-			System.Console.WriteLine("Nice to meet you, " + name + ".");
-		}
-	}
+  class HelloClass {
+    static void Main(string[] args) {
+      System.Console.WriteLine("Hello, what's your name?");
+      System.Console.Write("> ");
+      string name = System.Console.ReadLine();
+      System.Console.WriteLine("Nice to meet you, " + name + ".");
+    }
+  }
 }
